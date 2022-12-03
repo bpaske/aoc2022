b947f7686909f28826789b1b2458d64214f0fd83
Ben Paske <benpaske@bens-air.lan>
2022-12-03 16:45:19 +0000

    more elegant looping

--- 1/solution_generator.py
+++ 1/solution_generator.py
@@ -1,15 +1,13 @@
 import heapq
+import itertools
+
 
 def totals(filename):
-    with open (filename) as f:
-        current_load = 0
-        for line in f:
-            l = line.strip()
-            if l =='':
-                yield current_load
-                current_load = 0
-            else:
-                current_load += int(l)
-three_largest = heapq.nlargest(3, totals('./input.txt'))
+    with open(filename) as f:
+        while lines := list(itertools.takewhile(lambda x: x != "\n" and x != "", f)):
+            yield sum(int(l.strip()) for l in lines)
+
+
+three_largest = heapq.nlargest(3, totals("./input.txt"))
 print(max(three_largest))
 print(sum(three_largest))
